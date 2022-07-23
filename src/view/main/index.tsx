01038de4a665b866ed80149d0cc3d7ec753247cc
Jay <originxh@gmail.com>
2022-07-23 15:45:26 +0900

    add setInterval function

--- src/view/main/index.tsx
+++ src/view/main/index.tsx
@@ -1,22 +1,75 @@
 import { useHistory } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { OnChangeType, KeyDownType } from "../../types";
 
 const Main: React.FC = function () {
+    const randomNum = Math.random() * 10;
+    const randomArr = Math.floor(randomNum);
+    const Menu = [
+        "아메리카노(I)",
+        "아메리카노(H)",
+        "카페라떼(I)",
+        "카페라떼(H)",
+        "바닐라라떼(I)",
+        "헤이즐넛라떼(I)",
+        "돌체라떼(I)",
+        "카라멜마끼야또(I)",
+        "카페모카(I)",
+        "흑당카페라떼",
+    ];
+    const [downMenuNum, setDownMenuNUm] = useState(0);
+    const [rain, setRain] = useState(["10000", "22222", "3"]);
+    // 첫번째 단어 삽입.
+    // 1초마다 80px 다운.
+    // 반정도 내려오면 다음 단어 다운.
+    // 데드라인 충돌시 하트 삭제. 최근단어 삭제. 큐형식.
+    console.log(Menu[randomArr]);
     const history = useHistory();
-    const [heart, setHeart] = useState("❤️");
-    const [inputs, setInputs] = useState({ name: "" });
-    const [stack, setStack] = useState("");
+    const [heart, setHeart] = useState("❤️ ❤️ ❤️");
+    const [inputs, setInputs] = useState({ material: "" });
+    const [stack, setStack] = useState([undefined, "", ""]);
     const onChange: OnChangeType = function (e, state, setState) {
         setState({ ...state, [e.target.name]: e.target.value });
     };
     console.log(inputs);
+
+    // 1초마다 떨어지는 함수.
+    const dropDownMenu = function () {
+        let routine = setInterval(() => {
+            setDownMenuNUm((downMenuNum) => downMenuNum + 97);
+        }, 1000);
+        setTimeout(() => {
+            clearInterval(routine);
+            console.log("정지");
+        }, 5000);
+    };
+
+    useEffect(() => {
+        dropDownMenu();
+    }, []);
+    // + 데드라인 왔을 때 단어 off.
     const onKeyDownEnter: KeyDownType = function (e) {
         // console.log(e.code);
-        if (e.code === "Enter") {
-            setStack(inputs.name);
-            setInputs({ name: "" });
+        if (e.code === "Enter" && inputs.material.length > 1) {
+            setInputs({ material: "" });
+            for (let i = 0; i < 3; i++) {
+                if (stack[i] === stack[i + 1]) {
+                    console.log("중복되는 재료가 있습니다.");
+                    setInputs({ material: "" });
+                } else {
+                    if (stack[0] === undefined) {
+                        setStack([inputs.material]);
+                        setInputs({ material: "" });
+                    }
+                    if (stack[1] === undefined || stack[2] === undefined) {
+                        setStack([...stack, inputs.material]);
+                        setInputs({ material: "" });
+                    } else {
+                        setStack([stack[1], stack[2], inputs.material]);
+                    }
+                }
+            }
         }
     };
     return (
@@ -28,18 +81,28 @@ const Main: React.FC = function () {
                     <span>score: 0</span>
                     <span>{heart}</span>
                 </header>
-                <div className="block-down">커피 쏟아짐</div>
+                <div className="block-down-field">
+                    <p className="block" style={{ top: `${downMenuNum}px` }}>
+                        {Menu[randomArr]}
+                    </p>
+                    {/* <span
+                        className="raining"
+                    >
+                        {rain[0]}
+                    </span> */}
+                </div>
                 <ul className="stack">
-                    stack
-                    <li>{stack}</li>
-                    <li></li>
-                    <li></li>
+                    LIST
+                    <li>{stack[0]}</li>
+                    <li>{stack[1]}</li>
+                    <li>{stack[2]}</li>
                 </ul>
                 <footer>
+                    {/* 입력시 뒤에 숫자를 붙임 얼음100, 물200 샷1  */}
                     <input
                         onChange={(e) => onChange(e, inputs, setInputs)}
-                        name="name"
-                        value={inputs.name}
+                        name="material"
+                        value={inputs.material}
                         onKeyDown={onKeyDownEnter}
                         type="text"
                         placeholder="재료를 입력하세요"
